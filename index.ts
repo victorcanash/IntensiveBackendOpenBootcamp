e3a237f3b4085b7c40d25c63ca73d1c482db039c
Victor Canas <victorcanas972@gmail.com>
2022-04-04 19:19:28 +0200

    Adding main GET route

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express"
+import dotenv from 'dotenv'
 
 
 // Configuration the .env file
-dotenv.config();
+dotenv.config()
 
 // Create Express APP
 const app: Express = express();
@@ -11,19 +11,14 @@ const port: string | number = process.env.PORT || 8000;
 
 // Define the first Route of APP
 app.get('/', (req: Request, res: Response) => {
-    // Send Hello World
-    res.send('Welcome to my API Restful: Express + TS + Nodemon + Jest + Swagger + Mongoose');
-});
-
-// Define the first Route of APP
-app.get('/hello', (req: Request, res: Response) => {
-    // Send Hello World
-    res.send('Welcome to GET Route: ¡Hello!');
-});
-
+    const data = {
+        message: 'Goodbye, world'
+    }
+    res.send(data)
+})
 
 
 // Execute APP and Listen Requests to PORT
 app.listen(port, () => {
-    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
-});
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`)
+})
