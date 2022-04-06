fe83b0a24a8e55b5324f418fa9f313fd82eec96c
Victor Canas <victorcanas972@gmail.com>
2022-04-06 16:16:17 +0200

    Adding swagger

--- src/controllers/GoodbyeController.ts
+++ src/controllers/GoodbyeController.ts
@@ -1,11 +1,15 @@
+import { Get, Query, Route, Tags } from 'tsoa';
 import { DateResponse } from './types';
 import { IGoodbyeController } from './interfaces';
 import { LogSuccess } from '../utils/logger';
 
 
+@Route('/api/goodbye')
+@Tags('GoodbyeController')
 export class GoodbyeController implements IGoodbyeController {
 
-    public async getDateMessage(name?: string): Promise<DateResponse> {
+    @Get('/')
+    public async getDateMessage(@Query()name?: string): Promise<DateResponse> {
         LogSuccess('[/api/goodbye] Get Request');
 
         return {
--- src/controllers/HelloController.ts
+++ src/controllers/HelloController.ts
@@ -1,11 +1,15 @@
+import { Get, Query, Route, Tags } from 'tsoa';
 import { BasicResponse } from './types';
 import { IHelloController } from './interfaces';
 import { LogSuccess } from '../utils/logger';
 
 
+@Route('/api/hello')
+@Tags('HelloController')
 export class HelloController implements IHelloController {
     
-    public async getMessage(name?: string): Promise<BasicResponse> {
+    @Get('/')
+    public async getMessage(@Query()name?: string): Promise<BasicResponse> {
         LogSuccess('[/api/hello] Get Request');
 
         return {
