826b7419f88c41d10d0d81d7ece7a7e297c8fa37
Victor Canas <victorcanas972@gmail.com>
2022-04-12 14:04:43 +0200

    Adding IUser entity

--- src/domain/entities/User.entity.ts
+++ src/domain/entities/User.entity.ts
@@ -1,14 +1,18 @@
 import mongoose from 'mongoose';
 
+import { IUser } from '../interfaces/IUser.interface';
+
+
 export const userEntity = () => {
 
-    const userSchema = new mongoose.Schema(
+    const userSchema = new mongoose.Schema<IUser>(
         {
-            name: String,
-            email: String,
-            age: Number
+            name: { type: String, required: true },
+            email: { type: String, required: true },
+            password: { type: String, required: true },
+            age: { type: Number, required: true }
         }
     );
 
-    return mongoose.models.Users || mongoose.model('Users', userSchema);
+    return mongoose.models.Users || mongoose.model<IUser>('Users', userSchema);
 };
