5af3bfad55a76ea38727cb3433b026215878a6c2
Victor Canas <victorcanas972@gmail.com>
2022-04-26 17:08:33 +0200

    Cleaning types in auth orm

--- src/domain/orm/Auth.orm.ts
+++ src/domain/orm/Auth.orm.ts
@@ -1,29 +1,21 @@
-// Environment variables
 import dotenv from 'dotenv';
-// BCRYPT for passwords
 import bcrypt from 'bcrypt';
-// JWT
 import jwt from 'jsonwebtoken';
 
 import { userEntity } from '../entities/User.entity';
 import { IAuthLogin } from '../interfaces/IAuth.interface';
 import { IUser } from '../interfaces/IUser.interface';
+import { AuthResponse } from '../types';
 import { ModelNotFoundError, ErrorProviders, BadQueryError } from '../../errors';
 
 
-// Configuration of environment variables
 dotenv.config();
+const secret: string = process.env.SECRETKEY || 'MYSECRETKEY';
 
-// Obtain Secret key to generate JWT
-const secret = process.env.SECRETKEY || 'MYSECRETKEY';
-
-/**
- * Method to register/create a User from Collection "Users" passing its IUser in Mongo Server
- */
- export const registerUser = async (user: IUser): Promise<IUser | undefined> => {
+export const registerUser = async (user: IUser): Promise<IUser> => {
     const userModel = userEntity();
 
-    let createdUser: IUser | undefined = undefined; 
+    let createdUser: IUser = {} as IUser;
 
     await userModel.findOne({ email: user.email }).then((userResult: IUser) => {
         if (userResult) {
@@ -47,18 +39,18 @@ const secret = process.env.SECRETKEY || 'MYSECRETKEY';
     return createdUser;
 };
 
-/**
- * Method to login a User from Collection "Users" passing its IAuth in Mongo Server
- */
-export const loginUser = async (auth: IAuthLogin): Promise<{user: IUser | undefined, token: string | undefined}> => {
+
+export const loginUser = async (auth: IAuthLogin): Promise<AuthResponse> => {
     const userModel = userEntity();
 
-    let userFound: IUser | undefined = undefined;
-    let token: string | undefined = undefined;
+    const response = {
+        user: {} as IUser,
+        token: ''
+    } as AuthResponse;
 
     await userModel.findOne({ email: auth.email }).then((userResult: IUser) => {
-        userFound = userResult;
-        if (!userFound) {
+        response.user = userResult;
+        if (!response.user) {
             throw new ModelNotFoundError(ErrorProviders.AUTH, 'Invalid email to login');
         }
     }).catch((error: ModelNotFoundError) => {
@@ -66,7 +58,7 @@ export const loginUser = async (auth: IAuthLogin): Promise<{user: IUser | undefi
         throw error;
     });
 
-    const validPassword = bcrypt.compareSync(auth.password, userFound!.password);
+    const validPassword = bcrypt.compareSync(auth.password, response.user.password);
 
     if (!validPassword) {
         const passwordError = new ModelNotFoundError(ErrorProviders.AUTH, 'Invalid password to login');
@@ -74,20 +66,14 @@ export const loginUser = async (auth: IAuthLogin): Promise<{user: IUser | undefi
         throw passwordError;
     }
     
-    token = jwt.sign({ email: userFound!.email }, secret, {
+    response.token = jwt.sign({ email: response.user.email }, secret, {
         expiresIn: '24h'
     });
 
-    const result = {
-        user: userFound,
-        token: token
-    };
-    return result;
+    return response;
 };
 
-/**
- * Method to logout the current connected user in Mongod Server
- */
-export const logoutUser = async (): Promise<any | undefined> => {
+
+export const logoutUser = async (): Promise<any> => {
     // TODO: NOT IMPLEMENTED
 };
