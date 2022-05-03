944ca1d4a793f902fe8f5b9d3bca8b11b6925719
Victor Canas <victorcanas972@gmail.com>
2022-05-03 11:41:37 +0200

    Fixing controller interfaces

--- src/controllers/interfaces/index.ts
+++ src/controllers/interfaces/index.ts
@@ -1,7 +1,9 @@
+import { Request } from 'express';
+
 import { IUserUpdate } from '../../domain/interfaces/IUser.interface';
 import { IAuthLogin, IAuthRegister } from '@/domain/interfaces/IAuth.interface';
 import { IKataStars, IKataUpdate, KataLevel } from '../../domain/interfaces/IKata.interface';
-import { BasicResponse, DateResponse } from '../types';
+import { BasicResponse, DateResponse, AuthResponse, UserResponse, UsersResponse, KatasFromUserResponse, KataResponse, KatasResponse, ErrorResponse } from '../types';
 
 
 export interface IHelloController {
@@ -13,38 +15,41 @@ export interface IGoodbyeController {
 }
 
 export interface IUsersController {
-    // Get all users from database with pagination || Get User By ID
-    getUsers(page: number, limit: number, order: any, id?: string): Promise<any>
-    // Delete User By ID
-    deleteUser(id?: string): Promise<any>
-    // Update user
-    updateUser(user: IUserUpdate, id?: string): Promise<any>
-    // Get Katas of User with pagination
-    getKatas(page: number, limit: number, order: any, id?: string, level?: KataLevel): Promise<any>
+    // Get all users from database with pagination || Get user by id
+    getUsers(page: number, limit: number, order: any, id?: string): Promise<UserResponse | UsersResponse | ErrorResponse>
+    // Delete logged user
+    deleteUser(): Promise<BasicResponse | ErrorResponse>
+    // Update logged user
+    updateUser(user: IUserUpdate): Promise<BasicResponse | ErrorResponse>
+    // Get Katas of user with pagination
+    getKatas(page: number, limit: number, order: any, id: string, level?: KataLevel): Promise<KatasFromUserResponse | ErrorResponse>
 }
 
 export interface IAuthController {
     // Register users
-    registerUser(auth: IAuthRegister): Promise<any>
+    registerUser(auth: IAuthRegister): Promise<BasicResponse | ErrorResponse>
     // Login user
-    loginUser(auth: IAuthLogin): Promise<any>
+    loginUser(auth: IAuthLogin): Promise<AuthResponse | ErrorResponse>
     // Get logged user
-    getLoggedUser(id: string): Promise<any>
+    getLoggedUser(id: string): Promise<UserResponse | ErrorResponse>
     // Logout user
-    logoutUser(): Promise<any>
+    logoutUser(request: Request): Promise<BasicResponse | ErrorResponse>
 }
 
 export interface IKatasController {
     // Get all katas from database with pagination || Get kata By ID
-    getKatas(page: number, limit: number, order: any, id?: string, level?: KataLevel): Promise<any>
+    getKatas(page: number, limit: number, order: any, id?: string, level?: KataLevel): Promise<KataResponse | KatasResponse | ErrorResponse>
     // Create New Kata
-    createKata(kata: IKataUpdate, userId: string): Promise<any>
+    createKata(kata: IKataUpdate): Promise<BasicResponse | ErrorResponse>
     // Delete Kata By ID
-    deleteKata(id?: string, userId?: string): Promise<any>
+    deleteKata(id: string): Promise<BasicResponse | ErrorResponse>
     // Update Kata
-    updateKata(kata: IKataUpdate, id?: string, userId?: string): Promise<any>
+    updateKata(kata: IKataUpdate, id: string): Promise<BasicResponse | ErrorResponse>
     // Update Kata Stars
-    updateKataStars(kataStars: IKataStars, id?: string): Promise<any>
+    updateKataStars(kataStars: IKataStars, id: string): Promise<BasicResponse | ErrorResponse>
     // Check Kata Solution
-    sendKataSolution(solution: string, id?: string, userId?: string): Promise<any>
+    sendKataSolution(solution: string, id: string): Promise<BasicResponse | ErrorResponse>
+    // Update kata file
+    // eslint-disable-next-line no-undef
+    updateKataFile(file: Express.Multer.File): Promise<BasicResponse | ErrorResponse>
 }
