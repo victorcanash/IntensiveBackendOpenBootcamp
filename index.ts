ee141357a1e318d24b0dae8d8516af3e4123e191
Victor Canas <victorcanas972@gmail.com>
2022-05-10 21:57:23 +0200

    Deleting HOST env

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { envConfig } from './src/config/env.config';
 import { initMongo } from './src/domain/repositories/mongo.repo';
 
 console.log('STARTING SERVER', +envConfig.PORT);
-server.listen(+envConfig.PORT, envConfig.HOST, () => {
+server.listen(+envConfig.PORT, () => {
     LogSuccess(`[SERVER ON]: Running in http://${envConfig.HOST}:${envConfig.PORT}/api`);
 });
 
