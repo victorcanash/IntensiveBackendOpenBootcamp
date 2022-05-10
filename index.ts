3b55bda86795b26b45762ce864fb92958a0bcf45
Victor Canas <victorcanas972@gmail.com>
2022-05-10 16:53:58 +0200

    Adding node version in package

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { LogError, LogSuccess } from './src/utils/logger';
 import { envConfig } from './src/config/env.config';
 import { initMongo } from './src/domain/repositories/mongo.repo';
 
-
+console.log('STARTING SERVER', +envConfig.PORT);
 server.listen(+envConfig.PORT, envConfig.HOST, () => {
     LogSuccess(`[SERVER ON]: Running in http://${envConfig.HOST}:${envConfig.PORT}/api`);
 });
